fe0b17a44a2bbcb83c29e620264f1bfd4ca51bf4
Tom <twdickman@gmail.com>
2023-12-20 09:55:32 +1100

    fix: Remove env var check
    - Env var check was causing deployment issues
    - If env var is missing, app will throw error

--- apps/web/app/auth.ts
+++ apps/web/app/auth.ts
@@ -4,13 +4,8 @@ import Auth0 from "next-auth/providers/auth0"
 const {
   AUTH_AUTH0_CLIENT_ID,
   AUTH_AUTH0_CLIENT_SECRET,
-  AUTH_SECRET,
-  AUTH_URL
+  AUTH_SECRET
 } = process.env
-
-if (!AUTH_AUTH0_CLIENT_ID || !AUTH_AUTH0_CLIENT_SECRET || !AUTH_SECRET || !AUTH_URL) {
-  throw new Error('Missing environment variables')
-}
 
 export const {
   handlers: { GET, POST },
@@ -20,5 +15,5 @@ export const {
     clientId: AUTH_AUTH0_CLIENT_ID,
     clientSecret: AUTH_AUTH0_CLIENT_SECRET,
   })],
-  secret: process.env.AUTH_SECRET
+  secret: AUTH_SECRET
 })
