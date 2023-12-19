238df59586cadade35a2af943a75cc067ec97e1e
Tom <twdickman@gmail.com>
2023-12-20 10:01:08 +1100

    fix: Add issuer to Auth0 provider

--- apps/web/app/auth.ts
+++ apps/web/app/auth.ts
@@ -4,6 +4,7 @@ import Auth0 from "next-auth/providers/auth0"
 const {
   AUTH_AUTH0_CLIENT_ID,
   AUTH_AUTH0_CLIENT_SECRET,
+  AUTH_AUTH0_ISSUER,
   AUTH_SECRET
 } = process.env
 
@@ -14,6 +15,7 @@ export const {
   providers: [Auth0({
     clientId: AUTH_AUTH0_CLIENT_ID,
     clientSecret: AUTH_AUTH0_CLIENT_SECRET,
+    issuer: AUTH_AUTH0_ISSUER,
   })],
   secret: AUTH_SECRET
 })
