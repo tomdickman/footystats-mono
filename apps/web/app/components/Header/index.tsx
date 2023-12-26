9d6e6ff45f19cde4f89a0236b6dc03c2f66c4e0a
Tom <twdickman@gmail.com>
2023-12-27 08:25:44 +1100

    fix: Header left side gap

--- apps/web/app/components/Header/index.tsx
+++ apps/web/app/components/Header/index.tsx
@@ -4,8 +4,10 @@ export async function Header(): Promise<JSX.Element> {
   const session = await auth()
 
   return (
-    <header className='container py-2 flex items-center w-100 mx-auto'>
-      <a className='text-md leading-5 font-semibold hover:underline' href='/'>Home</a>
+    <header className='container p-2 flex items-center w-100 mx-auto'>
+      <div className='flex items-center gap-1.5'>
+        <a className='text-md leading-5 font-semibold hover:underline' href='/'>Home</a>
+      </div>
       <div className='flex items-center justify-end gap-1.5 ml-auto p-1'>
         <a className='text-sm leading-5 font-semibold bg-zinc-400/10 rounded-full py-1 px-3 flex items-center space-x-2 hover:bg-zinc-400/20 dark:highlight-white/5' href={`/api/auth/${session ? 'signout' : 'signin'}`}>{session ? 'Logout' : 'Login'}</a>
       </div>
