72b182d3e62e27c60a73e60d08852e721ec292c9
Tom <twdickman@gmail.com>
2023-12-27 12:11:52 +1100

    feat: Add external year filter

--- apps/web/app/components/PlayerStatsTable/table.tsx
+++ apps/web/app/components/PlayerStatsTable/table.tsx
@@ -3,7 +3,7 @@
 import type { GridOptions, ColDef } from 'ag-grid-community';
 import { AgGridReact } from 'ag-grid-react'
 import { type RoundStats } from 'database'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 
 import 'ag-grid-community/styles/ag-grid.css'
 import 'ag-grid-community/styles/ag-theme-alpine.css'
@@ -11,31 +11,106 @@ import 'ag-grid-community/styles/ag-theme-alpine.css'
 export function Table({
   roundStats
 }: { roundStats: RoundStats[] }): JSX.Element {
+  const [filteredRoundStats, setFilteredRoundStats] = useState<RoundStats[]>(roundStats)
+
   const colDefs = useMemo<ColDef[]>(() => [
-    { field: 'game', headerName: 'Game', headerTooltip: 'Career Game Number' },
-    { field: 'opponent', headerName: 'Opponent', headerTooltip: 'Opponent' },
-    { field: 'roundnumber', headerName: 'Round', headerTooltip: 'Season Round Number' },
-    { field: 'year', headerName: 'Year', headerTooltip: 'Year' },
-    { field: 'handballs', headerName: 'HB', headerTooltip: 'Handballs' },
-    { field: 'kicks', headerName: 'K', headerTooltip: 'Kicks' },
-    { field: 'tackles', headerName: 'T', headerTooltip: 'Tackles' },
-    { field: 'freekicksagainst', headerName: 'FA', headerTooltip: 'Free Kicks Against' },
-    { field: 'freekicksfor', headerName: 'FF', headerTooltip: 'Free Kicks For' },
-    { field: 'goals', headerName: 'G', headerTooltip: 'Goals' },
-    { field: 'behinds', headerName: 'B', headerTooltip: 'Behinds' },
-    { field: 'fantasypoints', headerName: 'Pts', headerTooltip: 'Fantasy Points' },
+    {
+      field: 'game',
+      headerName: 'Game',
+      headerTooltip: 'Career Game Number',
+      sort: 'desc'
+    },
+    {
+      field: 'opponent',
+      headerName: 'Opponent',
+      headerTooltip: 'Opponent'
+    },
+    {
+      field: 'roundnumber',
+      headerName: 'Round',
+      headerTooltip: 'Season Round Number'
+    },
+    {
+      field:'year',
+      headerName: 'Year',
+      headerTooltip: 'Year'
+    },
+    {
+      field: 'fantasypoints',
+      headerName: 'Pts',
+      headerTooltip: 'Fantasy Points'
+    },
+    {
+      field: 'handballs',
+      headerName: 'HB',
+      headerTooltip: 'Handballs'
+    },
+    {
+      field: 'kicks',
+      headerName: 'K',
+      headerTooltip: 'Kicks'
+    },
+    {
+      field: 'tackles',
+      headerName: 'T',
+      headerTooltip: 'Tackles'
+    },
+    {
+      field: 'freekicksagainst',
+      headerName: 'FA',
+      headerTooltip: 'Free Kicks Against'
+    },
+    {
+      field: 'freekicksfor',
+      headerName: 'FF',
+      headerTooltip: 'Free Kicks For'
+    },
+    {
+      field: 'goals',
+      headerName: 'G',
+      headerTooltip: 'Goals'
+    },
+    {
+      field: 'behinds',
+      headerName: 'B',
+      headerTooltip: 'Behinds'
+    },
   ], [])
 
   const gridOptions = useMemo<GridOptions>(() => ({
     autoSizeStrategy: {
       type: 'fitGridWidth',
-      defaultMinWidth: 75
+      defaultMinWidth: 100
+    },
+    defaultColDef: {
+      filter: true,
+      floatingFilter: true,
+      suppressMenu: true,
     }
   }), [])
 
   return (
-    <div className='ag-theme-alpine-auto-dark' style={{ height: '60dvh' }}>
-      <AgGridReact columnDefs={colDefs} gridOptions={gridOptions} rowData={roundStats} />
-    </div>
+    <>
+      <div className='container py-2'>
+        <select className='border-2 border-solid border-black dark:border-zinc-100/10 rounded-lg p-2 w-100 text-black' onChange={(event) => {
+          const selectedYear = event.currentTarget.value
+          if (selectedYear === 'all') {
+            setFilteredRoundStats(roundStats)
+          } else {
+            setFilteredRoundStats(roundStats.filter((roundStat) => roundStat.year.toString() === selectedYear))
+          }
+        } }>
+          <option key='all' value='all'>All Years</option>
+          {Array.from(new Set(roundStats.map((roundStat) => roundStat.year))).map((year) => {
+            return (
+              <option key={year} value={year}>{year}</option>
+            )
+          })}
+        </select>
+      </div>
+      <div className='ag-theme-alpine-auto-dark' style={{ height: '60dvh' }}>
+        <AgGridReact columnDefs={colDefs} gridOptions={gridOptions} rowData={filteredRoundStats} />
+      </div>
+    </>
   )
 }
